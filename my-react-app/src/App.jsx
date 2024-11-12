c6cc06b6f33cef05fa804a25680ffa4d80e15656
Carl Wang <carl.w@wustl.edu>
2024-11-12 14:58:44 -0600

    added usestate and state variable for value for square

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,27 +1,32 @@
+import { useState } from "react";
+
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value={"1"} />
-        <Square value={"2"} />
-        <Square value={"3"} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={"4"} />
-        <Square value={"5"} />
-        <Square value={"6"} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={"7"} />
-        <Square value={"8"} />
-        <Square value={"9"} />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
 }
 
 // individual square 
-function Square({value}) {
+function Square() {
+
+  // define state vars
+  const [val, setVal] = useState(null);
 
   // handle click
   function handleClick(){
