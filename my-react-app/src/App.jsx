1d4e67f643ad18a86b93ded285d659a618e0e72d
Carl Wang <carl.w@wustl.edu>
2024-11-13 18:54:55 -0600

    added calculate winner function to check for winners

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,13 +5,13 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if(squares[i]){ return;} // don't fill something that's already filled
+    if (squares[i]) { return; } // don't fill something that's already filled
 
     const newSquares = squares.slice();
     if (xisNext) { // take turns 
       newSquares[i] = "X";
     }
-    else{
+    else {
       newSquares[i] = "O";
     }
     setSquares(newSquares);
@@ -40,6 +40,27 @@ export default function Board() {
 }
 
 // individual square 
-function Square({value, onSquareClick}) {
+function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
+
+// calculate winner
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) { // check winner to winning patterns 
+      return squares[a];
+    }
+  }
+  return null;
 }
